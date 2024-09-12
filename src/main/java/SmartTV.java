00776064829d5d77a90f6795b342358c88c76eec
Herman Lundby-Holen <hermanluho@gmail.com>
2024-09-12 11:26:43 +0200

    Created SmartTV and TVController classes with documentation.

---
+++ src/main/java/SmartTV.java
@@ -0,0 +1,13 @@
+/**
+ * A TCP server.
+ * <p>Turned off by default, and can <b>ONLY</b> be turned on when off.</p>
+ * <p>Can report whether on or off.</p>
+ * <p>When turned on it should be able to send the number of available channels,
+ * get the currently active channel (channel 1 is default), and set channel to desired number c.</p>
+ *
+ */
+public class SmartTV {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
